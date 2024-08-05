82c9065c2fc4958a8e12ee36d248285a2b2fdde9
Jonathan Bernal <jsbernalp@gmail.com>
2024-08-05 12:24:23 -0500

    new DockerFile added

---
+++ init-db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Reemplaza las variables de entorno en el archivo init.sql y guarda el resultado en un nuevo archivo
+envsubst < /docker-entrypoint-initdb.d/init.sql > /docker-entrypoint-initdb.d/init.sql
+
+# Ejecuta el comando de inicialización original
+/docker-entrypoint.sh mysqld
